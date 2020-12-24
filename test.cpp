00f26159fbd4bf7aa2c3bde1429ee6afe8d6aebb
bck <buchangkai@163.com>
2020-12-24 18:03:44 +0800

    Write some words to encourage myself.

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    cout << "Hello World!" << endl;
+
+    cout << "Hello Christmas" << endl;
+
+    cout << "Come on ckBu!" << endl;
+
+    return 0;
+}
